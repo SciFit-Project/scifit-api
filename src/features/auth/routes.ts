55af9debfb9d1e988fb85e04e0907a9b01aef6b9
tony219y <aekanan2004@gmail.com>
2026-03-02 00:24:41 +0700

    refactor: improve error handling and response structure in auth routes and services

--- src/features/auth/routes.ts
+++ src/features/auth/routes.ts
@@ -1,40 +1,48 @@
-import { Hono } from 'hono'
-import { loginSchema, LoginSchema, RegisterInput, registerSchema } from './schema'
-import { registerUser, syncGoogleUser, userLogin } from './service'
-import { validate } from '../../core/middleware/validator'
-import { authMiddleware } from '../../core/middleware/auth'
+import { Hono } from "hono";
+import {
+  loginSchema,
+  LoginSchema,
+  RegisterInput,
+  registerSchema,
+} from "./schema";
+import { registerUser, syncGoogleUser, userLogin } from "./service";
+import { validate } from "../../core/middleware/validator";
+import { authMiddleware } from "../../core/middleware/auth";
 
-const auth = new Hono()
+const auth = new Hono();
 
 // register
-auth.post('/register', validate(registerSchema), async (c) => {
-    try {
-        const body = c.req.valid('json') as RegisterInput
-        const result = await registerUser(body);
-        return c.json({ success: true, data: result }, 201)
-    } catch (error) {
-        return c.json({ success: true, data: error }, 400)
-    }
-})
+auth.post("/register", validate(registerSchema), async (c) => {
+  try {
+    const body = c.req.valid("json") as RegisterInput;
+    const result = await registerUser(body);
+    return c.json({ success: true, ...result }, 201);
+  } catch (error: any) {
+    const status = error.status || 500;
+    const message = error.message || "Internal Server Error";
+    return c.json({ success: false, message: message }, status);
+  }
+});
 
-auth.post('/login', validate(loginSchema), async (c) => {
-    try {
-        const body = c.req.valid('json') as LoginSchema;
-        const result = await userLogin(body)
-        return c.json({ success: true, data: result }, 201)
-    } catch (error) {
-        return c.json({ success: true, data: error }, 400)
-    }
-})
-auth.post('/google-sync', async (c) => {
-    const body = await c.req.json();
-    const result = await syncGoogleUser(body);
-    return c.json({ success: true, data: result }, 201)
-})
+auth.post("/login", validate(loginSchema), async (c) => {
+  try {
+    const body = c.req.valid("json") as LoginSchema;
+    const result = await userLogin(body);
+    return c.json({ success: true, ...result }, 200);
+  } catch (error: any) {
+    const status = error.status || 500;
+    const message = error.message || "Internal Server Error";
+    return c.json({ success: false, message: message }, status);
+  }
+});
+auth.post("/google-sync", async (c) => {
+  const body = await c.req.json();
+  const result = await syncGoogleUser(body);
+  return c.json({ success: true, data: result }, 201);
+});
 
-auth.get('/test', authMiddleware, async (c)=>{
-    return c.json("validated")
-})
+auth.get("/test", authMiddleware, async (c) => {
+  return c.json("validated");
+});
 
-export default auth
-
+export default auth;
--- src/features/auth/service.ts
+++ src/features/auth/service.ts
@@ -1,93 +1,103 @@
-import { db } from '../../core/db'
-import { users } from '../../core/db/schema/schema'
-import { comparePassword, hashPassword } from './helper/hash'
-import type { LoginSchema, RegisterInput } from './schema'
-import { v4 as uuidv4 } from 'uuid';
-import { eq } from 'drizzle-orm';
-import { generateToken } from '../../core/middleware/auth';
-import { use } from 'hono/jsx';
+import { db } from "../../core/db";
+import { users } from "../../core/db/schema/schema";
+import { comparePassword, hashPassword } from "./helper/hash";
+import type { LoginSchema, RegisterInput } from "./schema";
+import { v4 as uuidv4 } from "uuid";
+import { eq } from "drizzle-orm";
+import { generateToken } from "../../core/middleware/auth";
 
 export const registerUser = async (body: RegisterInput) => {
-    const normalizedEmail = body.email.toLowerCase().trim();
+  const normalizedEmail = body.email.toLowerCase().trim();
 
-    try {
-        const existingUser = await db.select()
-            .from(users)
-            .where(eq(users.email, normalizedEmail))
-            .limit(1);
+  try {
+    const existingUser = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, normalizedEmail))
+      .limit(1);
 
-        if (existingUser.length > 0) {
-            return { error: "Email already in use", status: 400 };
-        }
-
-        const hashed = await hashPassword(body.password);
-        const userId = uuidv4();
-
-        const [create] = await db.insert(users).values({
-            id: userId,
-            email: normalizedEmail,
-            fullName: body.fullname,
-            password: hashed,
-            provider: "email"
-        }).returning({
-            id: users.id,
-            fullName: users.fullName,
-            email: users.email,
-            role: users.role
-        });
-
-        return { success: true, data: create };
-
-    } catch (error) {
-        console.error("Registration Error:", error);
-        return { error: "Internal Server Error", status: 500 };
+    if (existingUser.length > 0) {
+      throw { message: "Email already in use", status: 400 };
     }
-}
+
+    const hashed = await hashPassword(body.password);
+    const userId = uuidv4();
+
+    const [create] = await db
+      .insert(users)
+      .values({
+        id: userId,
+        email: normalizedEmail,
+        fullName: body.fullname,
+        password: hashed,
+        provider: "email",
+      })
+      .returning({
+        id: users.id,
+        fullName: users.fullName,
+        email: users.email,
+        role: users.role,
+      });
+
+    return { create };
+  } catch (error) {
+    console.error("Registration Error:", error);
+    throw { message: "Internal Server Error", status: 500 };
+  }
+};
 
 export const syncGoogleUser = async (body: any) => {
-    try {
-        await db.insert(users).values({
-            id: body.id,
-            email: body.email,
-            fullName: body.fullname,
-            avatarUrl: body.avatar,
-            provider: "google"
-        }).returning({
-            id: users.id,
-            fullName: users.fullName,
-            email: users.email,
-            role: users.role
-        });
+  try {
+    await db
+      .insert(users)
+      .values({
+        id: body.id,
+        email: body.email,
+        fullName: body.fullname,
+        avatarUrl: body.avatar,
+        provider: "google",
+      })
+      .returning({
+        id: users.id,
+        fullName: users.fullName,
+        email: users.email,
+        role: users.role,
+      });
 
-        return { success: true };
-
-    } catch (error) {
-        console.error("Registration Error:", error);
-        return { error: "Internal Server Error", status: 500 };
-    }
-}
+    throw { success: true };
+  } catch (error) {
+    console.error("Registration Error:", error);
+    throw { message: "Internal Server Error", status: 500 };
+  }
+};
 
 export const userLogin = async (body: LoginSchema) => {
-    const [user] = await db.select().from(users)
-        .where(eq(users.email, body.email)).limit(1);
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, body.email))
+    .limit(1);
 
-    if (!user) {
-        return { error: "User not found", status: 404 };
-    }
+  if (!user) {
+    throw { message: "User not found", status: 404 };
+  }
 
-    const isPasswordCorrect = await comparePassword(body.password, user.password!);
-    if (!isPasswordCorrect) {
-        return { error: "Invalid credentials", status: 401 };
-    }
-    if (!user.role) {
-        return { error: "Invalid credentials", status: 401 };
-    }
+  const isPasswordCorrect = await comparePassword(
+    body.password,
+    user.password!,
+  );
+  if (!isPasswordCorrect) {
+    throw { message: "Invalid credentials", status: 401 };
+  }
+  if (!user.role) {
+    throw { message: "Invalid credentials", status: 401 };
+  }
 
-    // Generate token
-    const token = await generateToken({
-        id: user.id,
-        email: user.email,
-        role: user.role
-    });
-    return { success: true, data: token };
-}
+  // Generate token
+  const token = await generateToken({
+    id: user.id,
+    email: user.email,
+    role: user.role,
+  });
+  return { token };
+};

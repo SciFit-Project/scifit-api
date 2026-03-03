3c5b2bdf7f9b665e3a5257ce12f3c79ece6251f4
tony219y <aekanan2004@gmail.com>
2026-03-03 23:42:06 +0700

    test deploy 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { handle } from "hono/vercel";
-import { app } from "..";
-// import { app } from "../index.js";
+// import { app } from "..";
+import { app } from "../index.js";
 
 export default handle(app);
